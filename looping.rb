bdc1a8a1662b817c47c751d521f88d5e5f63b632
Cheryl K <cherylkelson@icloud.com>
2018-09-11 19:36:38 +0000

    automatically backed up by learn

--- looping.rb
+++
@@ -1,4 +0,0 @@
-This is the first in a series of short exercises to help you get the hang of looping constructs.
-
-Fork and clone this lab.
-There are no tests for this lab! Code your solution in looping.rb:
